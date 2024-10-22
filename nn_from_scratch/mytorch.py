c8bece85efc5256963a2de1883ea320ff948f5c2
Albert Sun <albertsun0@gmail.com>
2024-10-21 22:06:04 -0700

    Add mytorch

---
+++ nn_from_scratch/mytorch.py
@@ -0,0 +1,193 @@
+import math
+import random
+
+# import matplotlib.pyplot as plt
+# from graphviz import Digraph
+
+
+class Value:
+    def __init__(self, data, _children=(), _op="", label=""):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+        self._backward = lambda: None
+        self.grad = 0.0
+        self._label = label
+
+    def __repr__(self):
+        # tostring() in python
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), "+")
+
+        # backward propogates current gradient backwards to direct children
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __radd__(self, other):
+        return self + other
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def exp(self):  # e^x
+        x = self.data
+        out = Value(math.exp(x), (self,), "exp")
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data**other, (self,), f"**{other}")
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Value(t, (self,), "tanh")
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def relu(self):
+        x = self.data
+        out = Value(max(0, x), (self,), "relu")
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        # build topological order so that we can backprop all parents/further nodes before children
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+            topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)  # sum default value
+        out = act.tanh()
+        return out
+
+    def parameters(self):
+        return self.w + [self.b]
+
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+
+
+class MLP:
+    def __init__(self, nin, nouts):  # in layers, list of output sizes
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+
+# def trace(n):
+#     nodes, edges = set(), set()
+
+#     def build(v):
+#         if v not in nodes:
+#             nodes.add(v)
+#             for child in v._prev:
+#                 edges.add((child, v))
+#                 build(child)
+
+#     build(n)
+#     return nodes, edges
+
+
+# def draw_dot(root):
+#     dot = Digraph(format="svg", graph_attr={"rankdir": "LR"})
+#     nodes, edges = trace(root)
+#     for n in nodes:
+#         uid = str(id(n))
+#         dot.node(
+#             uid,
+#             label=f"{n._label} | data {n.data:.4f} | grad {n.grad:.4f}",
+#             shape="record",
+#         )
+#         if n._op:
+#             dot.node(uid + n._op, label=n._op)
+#             dot.edge(uid + n._op, uid)
+#     for n1, n2 in edges:
+#         uid1, uid2 = str(id(n1)), str(id(n2))
+#         dot.edge(uid1, uid2 + n2._op)
+#     return dot
